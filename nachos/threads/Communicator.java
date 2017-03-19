5eee9c287be86989ce17118645075572e6bb6339
Shuran Zheng <Shuran Zheng>
2017-03-19 19:26:58 +0800

    Task IV added: See Communicator.java

--- nachos/threads/Communicator.java
+++ nachos/threads/Communicator.java
@@ -10,6 +10,13 @@ import nachos.machine.*;
  * threads can be paired off at this point.
  */
 public class Communicator {
+	
+	Lock lock = new Lock();
+	Condition waitingListener = new Condition(lock);
+	Condition activeListener = new Condition(lock);
+	Condition activeSpeaker = new Condition(lock);
+	Condition waitingSpeaker = new Condition(lock);
+	int word = 0, listener = 0, speaker = 0;
     /**
      * Allocate a new communicator.
      */
@@ -27,6 +34,15 @@ public class Communicator {
      * @param	word	the integer to transfer.
      */
     public void speak(int word) {
+    	lock.acquire();
+    	while (speaker > 0){
+    		waitingSpeaker.sleep();
+    	}
+    	speaker ++;
+    	this.word = word;
+    	if (listener > 0) activeListener.wake();
+    	else activeSpeaker.sleep();
+    	lock.release();
     }
 
     /**
@@ -36,6 +52,22 @@ public class Communicator {
      * @return	the integer transferred.
      */    
     public int listen() {
-	return 0;
+    	int ret = 0;
+    	lock.acquire();
+    	while (listener > 0){
+    		waitingListener.sleep();
+    	}
+    	listener ++;
+    	while (speaker <= 0){
+    		activeListener.sleep();
+    	}
+    	ret = word;
+    	speaker --;
+    	listener --;
+    	waitingSpeaker.wake();
+    	waitingListener.wake();
+    	activeSpeaker.wake();
+    	lock.release();
+    	return ret;
     }
 }
